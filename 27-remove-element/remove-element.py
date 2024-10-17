448205d8c13ec6d556eb39532b44fd752c2f71da
José Mathias <116528288+JoMath363@users.noreply.github.com>
2024-10-17 17:59:03 -0300

    Time: 3 ms (99.97%) | Memory: 11.5 MB (62.00%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -6,8 +6,10 @@ class Solution(object):
         :rtype: int
         """
 
-        while val in nums:
-            nums.remove(val)
-
-        return len(nums)
+        k = 0 
+        for i in range(len(nums)):
+            if nums[i] != val:  
+                nums[k] = nums[i]  
+                k += 1  
+        return k
         
