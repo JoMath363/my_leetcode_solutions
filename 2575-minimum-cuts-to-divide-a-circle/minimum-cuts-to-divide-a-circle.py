5ce80ab3f7139e437cc88916679c1801ddfb941e
José Mathias <116528288+JoMath363@users.noreply.github.com>
2024-10-21 22:58:47 -0300

    Time: 0 ms (100.00%) | Memory: 11.4 MB (84.62%) - LeetSync

--- 2575-minimum-cuts-to-divide-a-circle/minimum-cuts-to-divide-a-circle.py
+++ 2575-minimum-cuts-to-divide-a-circle/minimum-cuts-to-divide-a-circle.py
@@ -6,7 +6,6 @@ class Solution(object):
         """
         if n == 1:
             return 0
-        elif n % 2 == 0:
+        if n % 2 == 0:
             return n / 2
-        else:
-            return n
+        return n
