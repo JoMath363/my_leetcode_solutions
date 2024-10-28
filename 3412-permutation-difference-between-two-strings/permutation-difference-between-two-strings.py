bfe157bdc1afe424dc5967843f14bf5c774938ee
José Mathias <116528288+JoMath363@users.noreply.github.com>
2024-10-28 11:19:41 -0300

    Time: 4 ms (21.67%) | Memory: 16.7 MB (11.59%) - LeetSync

--- 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.py
+++ 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.py
@@ -1,13 +1,12 @@
 class Solution:
     def findPermutationDifference(self, s: str, t: str) -> int:
-        sMap = {}
-        tMap = {}
-
+        sMap, tMap = {}, {}
+        diff = 0
+        
         for i in range(len(s)):
             sMap[s[i]] = i
             tMap[t[i]] = i
-
-        diff = 0
+        
         for char in s:
             diff += abs(sMap[char] - tMap[char])
 
