8ae36a43437d4d3248dd647144cf84e1836810bf
José Mathias <116528288+JoMath363@users.noreply.github.com>
2024-11-01 08:12:51 -0300

    Time: 8 ms (22.13%) | Memory: 16.8 MB (27.97%) - LeetSync

--- 2881-split-strings-by-separator/split-strings-by-separator.py
+++ 2881-split-strings-by-separator/split-strings-by-separator.py
@@ -1,8 +1,9 @@
 class Solution:
     def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
-        res = []
-
-        for w in words:
-            res = res + w.split(separator)
-
-        return [x for x in res if not x  == '']
+        array = []
+        for word in words:
+            parts = word.split(separator)
+            for part in parts:
+                if part != '':
+                    array.append(part)
+        return array
