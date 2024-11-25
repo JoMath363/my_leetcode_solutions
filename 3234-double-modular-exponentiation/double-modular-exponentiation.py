ebe76b6f3a5bc49e7d648b0a108f98a9768fdd60
José Mathias <116528288+JoMath363@users.noreply.github.com>
2024-11-25 20:16:19 -0300

    Time: 1 ms (74.04%) | Memory: 16.8 MB (52.20%) - LeetSync

--- 3234-double-modular-exponentiation/double-modular-exponentiation.py
+++ 3234-double-modular-exponentiation/double-modular-exponentiation.py
@@ -2,9 +2,7 @@ class Solution:
     def getGoodIndices(self, variables: List[List[int]], target: int) -> List[int]:
         res = []
 
-        for i in range(len(variables)):
-            a, b, c, m = variables[i]
-
+        for i, (a, b, c, m) in enumerate(variables):
             if (((a ** b) % 10) ** c) % m == target:
                 res.append(i)
 
