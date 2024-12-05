cea1116b1da4bcaf2ecd698e3cafa6fbd6c1dc7c
José Mathias <116528288+JoMath363@users.noreply.github.com>
2024-12-05 19:52:58 -0300

    Time: 0 ms (100.00%) | Memory: 17.5 MB (11.04%) - LeetSync

--- 2226-rings-and-rods/rings-and-rods.py
+++ 2226-rings-and-rods/rings-and-rods.py
@@ -1,13 +1,8 @@
 class Solution:
     def countPoints(self, rings: str) -> int:
-        rods = defaultdict(set)
-        res = 0
-
-        for i in range(0, len(rings), 2):
-            rods[rings[i+1]].add(rings[i])
-
-        for i in rods:
-            if len(rods[i]) == 3:
-                res += 1
-
-        return res
+        ans = 0
+        for i in range(10):
+            i = str(i)
+            if 'R'+i in rings and 'G'+i in rings and 'B'+i in rings:
+                ans += 1
+        return ans
